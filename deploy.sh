39f8d634dea919d7f543a2763e016879ee006698
Lenon Marcel <lenon.marcel@gmail.com>
2020-10-01 22:44:17 -0300

    Deploy to gh-pages

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# can't use /tmp with github actions for some reason
+outputdir="$RUNNER_TEMP/build"
+mkdir -p "$outputdir"
+
+# let's build the site using hugo and current branch
+hugo --verbose --destination "$outputdir"
+
+# remove submodule and checkout to the branch used for github pages
+git submodule deinit .
+git checkout gh-pages
+
+# sync new changes and remove stale files
+# keep .git intact because we need to commit files later
+rsync --verbose \
+      --archive \
+      --delete \
+      --exclude .git \
+      "$outputdir/" .
+
+# add changes, commit and push
+git add -- .
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+git commit --message "automatically update site"
+git push origin gh-pages
