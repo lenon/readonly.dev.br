d1101ba2dd33715fec50e711d275a6e54b10b313
Lenon Marcel <lenon.marcel@gmail.com>
2020-10-25 14:24:49 -0300

    Replace checkout by switch

--- publish.sh
+++ publish.sh
@@ -5,15 +5,20 @@ set -euxo pipefail
 hugo env
 hugo config
 
-sitedir="$HOME/site-build"
-mkdir -p "$sitedir"
+builddir="$RUNNER_TEMP/hugo-build"
+mkdir -p "$builddir"
 
 # assuming we are on main branch
 # let's build the static site using Hugo
-hugo --verbose --destination "$sitedir"
+hugo --verbose --destination "$builddir"
 
-# gh-pages contains only static files and does not include Hugo source files
-git checkout --recurse-submodules gh-pages
+# create a new orphan branch for gh-pages if it does not exist yet
+# otherwise just track the remote branch
+if ! git show-ref --verify --quiet refs/remotes/origin/gh-pages ; then
+  git switch --recurse-submodules --orphan gh-pages
+else
+  git switch --recurse-submodules --create gh-pages --track origin/gh-pages
+fi
 
 # sync new changes and remove stale files
 # keep .git intact because we need to commit files later
@@ -23,7 +28,7 @@ rsync --verbose \
       --delete \
       --exclude .git \
       --exclude CNAME \
-      "$sitedir/" .
+      "$builddir/" .
 
 # add changes, commit and push
 # GitHub will then publish those pages
